e0d988bc11c1ab394be58f708318f81c31aa4885
Ramachandran T <tramachandran1@gmail.com>
2020-07-13 18:47:39 +0530

    server file changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/todo-list'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/todo-list/index.html'));});
+'/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
